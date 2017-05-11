92f2c2227678cef66345587923ca4a90440c75cd
Student <PKIRBZ@meatball.local>
2017-05-11 10:08:29 -0400

    getting the code started :I

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelDisplay: UILabel!
+    
+    //create an instance  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
